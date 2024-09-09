f836083311602f418776a18bc5f09cb777597cc6
rifabayuzul <rifabayuzulfanida@gmail.com>
2024-09-09 07:43:47 +0700

    update carousel.js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,16 @@
 import { addClass, removeClass } from "./utils-class";
 
 const carouselId = document.getElementById("carousel");
-const carouselId = document.getElementById("carousel");
+const carouselItems = carouselId.getElementsByClassName("flex")[0];
+const carouselContainer = carouselId.getElementsByClassName("container")[0];
+
+function carouselCalculatorOffset() {
+  const carouselOffset = carouselContainer.getBoundingClientRect().left;
+  carouselItems.style.paddingLeft = `${carouselOffset - 16}px`;
+  carouselItems.style.paddingRight = `${carouselOffset - 16}px`;
+}
+
+if (carouselId) {
+  window.addEventListener("load", carouselCalculatorOffset);
+  window.addEventListener("resize", carouselCalculatorOffset);
+}
